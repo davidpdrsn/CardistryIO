d6005e8294a616a6f19408a4f4b6f43b4fc78a04
David Pedersen <david.pdrsn@gmail.com>
2015-04-20 17:02:59 +0200

    Protect user#edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :require_login, only: [:edit, :update]
+  before_filter :has_access, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -15,5 +16,14 @@ class UsersController < ApplicationController
     @user.update(user_params)
     flash.notice = "Updated"
     redirect_to @user
+  end
+
+  private
+
+  def has_access
+    if current_user.id != params[:id].to_i
+      flash.alert = "Page not found"
+      redirect_to root_path
+    end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,11 +6,31 @@ describe UsersController do
       get :edit, id: 1
       expect(response.status).to eq 302
     end
+
+    it "only allows users to edit their own profile" do
+      bob = build_stubbed(:user)
+      alice = build_stubbed(:user)
+      sign_in_as bob
+
+      get :edit, id: alice.id
+
+      expect(response.status).to eq 302
+    end
   end
 
   describe "#update" do
     it "requires authentication" do
       patch :update, id: 1
+      expect(response.status).to eq 302
+    end
+
+    it "only allows users to edit their own profile" do
+      bob = build_stubbed(:user)
+      alice = build_stubbed(:user)
+      sign_in_as bob
+
+      patch :update, id: alice.id
+
       expect(response.status).to eq 302
     end
   end
