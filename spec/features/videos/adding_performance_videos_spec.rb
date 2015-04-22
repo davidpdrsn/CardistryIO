c674c315ef9524d1ea799cc0b2a1e918db8e287e
David Pedersen <david.pdrsn@gmail.com>
2015-04-22 14:36:20 +0200

    Change copy for submitting videos

--- spec/features/videos/adding_performance_videos_spec.rb
+++ spec/features/videos/adding_performance_videos_spec.rb
@@ -4,11 +4,11 @@ feature "adding performance videos" do
   scenario "adding video" do
     user = create :user
     visit root_path(as: user)
-    click_link "Create video"
+    click_link "Submit video"
     fill_in "Name", with: "Classic"
     fill_in "YouTube URL", with: "https://www.youtube.com/watch?v=W799NKLEz8s"
     fill_in "Description", with: "A video I made"
-    click_button "Create Video"
+    click_button "Submit video"
 
     admin = create :user, admin: true
     visit root_path(as: admin)
@@ -26,8 +26,8 @@ feature "adding performance videos" do
   scenario "adding invalid video" do
     user = create :user
     visit root_path(as: user)
-    click_link "Create video"
-    click_button "Create Video"
+    click_link "Submit video"
+    click_button "Submit video"
 
     expect(page).to have_content "can't be blank"
   end
