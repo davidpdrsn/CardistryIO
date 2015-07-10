7b74bfba5867dc04886419595cb36d86817923ff
bendobos <bendobos1@gmail.com>
2015-07-10 22:20:25 +0200

    Active-classes are now automatically added to the nav items when they are active

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    # Detects the current route and appends the active-nav-item class if the same one gets passed in
+    def cp(path)
+        current_route = Rails.application.routes.recognize_path(path)
+        "active-nav-item" if current_page?(path) or params[:controller] == current_route[:controller]
+    end
 end
