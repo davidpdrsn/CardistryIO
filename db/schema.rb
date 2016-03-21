51bfac184479b554d72047a25d6eb9c1810870ef
David Pedersen <david.pdrsn@gmail.com>
2016-03-21 23:19:01 +0100

    Setup binstubs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715203852) do
+ActiveRecord::Schema.define(version: 20160321213338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
