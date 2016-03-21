4eb033a3cbd5b0908a92a47dd1b2e502df019185
David Pedersen <david.pdrsn@gmail.com>
2016-03-21 22:15:54 +0100

    Even more deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:davidpdrsn/CardistryIO.git"
 
 set :deploy_to, "/home/cardistryio/cardistryio"
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :rbenv_type, :system
