ff51f3ac921e495cfc04a8808a0758e9263b71f2
David Pedersen <david.pdrsn@gmail.com>
2016-05-06 09:58:17 +0200

    Don't link bins between releases

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, "git@github.com:davidpdrsn/CardistryIO.git"
 set :deploy_to, "/home/cardistryio/cardistryio"
 
 set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :conditionally_migrate, true
 
