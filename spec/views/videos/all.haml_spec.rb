3612669ca83bcab5d0f43b70e601b0572735d054
David Pedersen <david.pdrsn@gmail.com>
2016-05-08 21:43:41 +0200

    Remove redundant test

--- spec/views/videos/all.haml_spec.rb
+++ spec/views/videos/all.haml_spec.rb
@@ -1,15 +1,6 @@
 require "rails_helper"
 
 describe "videos/all.haml" do
-  it "shows the videos" do
-    video = build_stubbed :video
-    assign :videos, [video]
-
-    render
-
-    expect(rendered).to match video.name
-  end
-
   it "shows when there are no videos" do
     assign :videos, []
 
