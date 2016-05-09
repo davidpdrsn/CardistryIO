5d613d1360e5e502818259cff5405bcbe9740cec
David Pedersen <david.pdrsn@gmail.com>
2016-05-09 13:52:17 +0200

    Remove redundant test file

--- spec/features/users/editing_profile_spec.rb
+++ spec/features/users/editing_profile_spec.rb
@@ -24,3 +24,22 @@ feature "editing profile" do
     expect(page).to have_content "New biography!"
   end
 end
+
+feature "link to edit profile" do
+  scenario "sees edit link" do
+    bob = create :user
+
+    visit user_path(bob, as: bob)
+
+    expect(page).to have_content "Edit"
+  end
+
+  scenario "doesn't see edit link if viewing another users profile" do
+    bob = create :user
+    alice = create :user
+
+    visit user_path(alice, as: bob)
+
+    expect(page).not_to have_content "Edit"
+  end
+end
