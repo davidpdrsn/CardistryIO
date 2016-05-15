2524d8d6d62914e29bf46c7a579d56cbe7a09bb1
David Pedersen <david.pdrsn@gmail.com>
2016-05-16 00:01:37 +0200

    Fix failing, redundant test
    Was redundant because this behavior is covered by several integration
    tests

--- spec/views/videos/index.haml_spec.rb
+++ spec/views/videos/index.haml_spec.rb
@@ -1,17 +1,8 @@
 require "rails_helper"
 
 describe "videos/index.haml" do
-  it "shows the videos" do
-    video = build_stubbed :video
-    assign :videos, [video]
-
-    render
-
-    expect(rendered).to match video.name
-  end
-
   it "shows when there are no videos" do
-    assign :videos, []
+    assign :paged_videos, []
 
     render
 
