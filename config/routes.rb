e43549d8ce6c7061dcd5af32198ddc02f229271e
David Pedersen <david.pdrsn@gmail.com>
2016-08-27 17:01:00 +0200

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :dashboard
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
