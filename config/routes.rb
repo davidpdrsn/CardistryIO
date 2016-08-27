d9296bf553561703607654521694a4cbc312f8ba
David Pedersen <david.pdrsn@gmail.com>
2016-08-27 17:41:01 +0200

    Add list that shows all users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, only: [:show, :edit, :update, :create] do
+  resources :users, only: [:index, :show, :edit, :update, :create] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
