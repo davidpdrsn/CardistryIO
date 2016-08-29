016be49bad82204767471b62ee3a25a572030741
David Pedersen <david.pdrsn@gmail.com>
2016-08-29 11:43:52 +0200

    Only show unread notifications in dropdown

---
+++ spec/features/notifications/marking_all_as_read_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+feature "marking all notifications as read" do
+  scenario "only shows the unread ones" do
+    huron = create :user, username: "huron"
+    daren = create :user, username: "daren"
+    josh = create :user, username: "josh"
+
+    visit user_path(huron, as: daren)
+    click_link "Follow"
+
+    visit root_path(as: huron)
+    click_link "Mark all as read"
+
+    visit user_path(huron, as: josh)
+    click_link "Follow"
+
+    visit root_path(as: huron)
+
+    expect(page).to_not have_content "daren started following you"
+    expect(page).to have_content "josh started following you"
+  end
+end
