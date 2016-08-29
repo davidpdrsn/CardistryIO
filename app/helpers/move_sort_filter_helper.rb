cf769a95a6ba5649dd3b0a1868326e206c57c1f4
David Pedersen <david.pdrsn@gmail.com>
2016-08-29 10:49:52 +0200

    Refactoring of `MoveSortFilterHelper`

--- app/helpers/move_sort_filter_helper.rb
+++ app/helpers/move_sort_filter_helper.rb
@@ -1,77 +1,56 @@
-# TODO: Fix duplication between this module and video related module
-# TODO: Don't require prefixing methods with move_
 module MoveSortFilterHelper
   def moves_filter_select(form)
+    current_filter_params = params.fetch(:filter, {}).fetch(:type, "all")
+    filter_options = options_for_select(
+      [
+        ["Show all", "all"],
+        ["Show only finished", "finished"],
+        ["Show only ideas", "idea"],
+      ],
+      current_filter_params,
+    )
+
     form.select(
       :type,
-      moves_filter_options,
+      filter_options,
       {},
       { class: "select", onchange: "this.form.submit()" },
     )
   end
 
   def moves_sort_by_select(form)
+    current_sort_by_options = params.fetch(:sort, {}).fetch(:by, "created_at")
+    sort_by_options = options_for_select(
+      [
+        ["Sort by date", "created_at"],
+        ["Sort by rating", "rating"],
+      ],
+      current_sort_by_options,
+    )
+
     form.select(
       :by,
-      moves_sort_by_options,
+      sort_by_options,
       {},
       { class: "select", onchange: "this.form.submit()" },
     )
   end
 
   def moves_sort_direction_select(form)
-    form.select(
-      :direction,
-      moves_sort_direction_options,
-      {},
-      { class: "select", onchange: "this.form.submit()" },
-    )
-  end
-
-  private
-
-  def moves_current_filter_params
-    params.fetch(:filter, {}).fetch(:type, "all")
-  end
-
-  def moves_filter_options
-    options_for_select(
-      [
-        ["Show all", "all"],
-        ["Show only finished", "finished"],
-        ["Show only ideas", "idea"],
-      ],
-      moves_current_filter_params,
-    )
-  end
-
-  def moves_current_sort_by_options
-    params.fetch(:sort, {}).fetch(:by, "created_at")
-  end
-
-  def moves_sort_by_options
-    options_for_select(
-      [
-        ["Sort by date", "created_at"],
-        ["Sort by rating", "rating"],
-      ],
-      moves_current_sort_by_options,
-    )
-  end
-
-  # DUP: laksjdlk
-  def moves_sort_direction_options
-    options_for_select(
+    current_sort_direction_options = params.fetch(:sort, {}).fetch(:direction, "DESC")
+    sort_direction_options = options_for_select(
       [
         ["Descending", "DESC"],
         ["Ascending", "ASC"]
       ],
-      moves_current_sort_direction_options,
+      current_sort_direction_options,
     )
-  end
 
-  # DUP: laksjdlk2
-  def moves_current_sort_direction_options
-    params.fetch(:sort, {}).fetch(:direction, "DESC")
+    form.select(
+      :direction,
+      sort_direction_options,
+      {},
+      { class: "select", onchange: "this.form.submit()" },
+    )
   end
 end
