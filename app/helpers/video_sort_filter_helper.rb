f279e8446743ffc11713708e66eb1dabf9652c81
David Pedersen <david.pdrsn@gmail.com>
2016-08-29 10:53:33 +0200

    Refactor `VideoSortFilterHelper`

--- app/helpers/video_sort_filter_helper.rb
+++ app/helpers/video_sort_filter_helper.rb
@@ -1,5 +1,8 @@
 module VideoSortFilterHelper
   def videos_filter_select(form)
+    current_filter_params = params.fetch(:filter, {}).fetch(:type, "all")
+    filter_options = options_for_select(Video.types_for_filtering, current_filter_params)
+
     form.select(
       :type,
       filter_options,
@@ -9,6 +12,16 @@ module VideoSortFilterHelper
   end
 
   def videos_sort_by_select(form)
+    current_sort_by_options = params.fetch(:sort, {}).fetch(:by, "created_at")
+    sort_by_options = options_for_select(
+      [
+        ["Sort by date", "created_at"],
+        ["Sort by rating", "rating"],
+        ["Sort by views", "views_count"],
+      ],
+      current_sort_by_options,
+    )
+
     form.select(
       :by,
       sort_by_options,
@@ -18,52 +31,20 @@ module VideoSortFilterHelper
   end
 
   def videos_sort_direction_select(form)
-    form.select(
-      :direction,
-      sort_direction_options,
-      {},
-      { class: "select", onchange: "this.form.submit()" },
-    )
-  end
-
-  private
-
-  def current_filter_params
-    params.fetch(:filter, {}).fetch(:type, "all")
-  end
-
-  def filter_options
-    options_for_select(Video.types_for_filtering, current_filter_params)
-  end
-
-  def current_sort_by_options
-    params.fetch(:sort, {}).fetch(:by, "created_at")
-  end
-
-  def sort_by_options
-    options_for_select(
-      [
-        ["Sort by date", "created_at"],
-        ["Sort by rating", "rating"],
-        ["Sort by views", "views_count"],
-      ],
-      current_sort_by_options,
-    )
-  end
-
-  # DUP: laksjdlk
-  def sort_direction_options
-    options_for_select(
+    current_sort_direction_options = params.fetch(:sort, {}).fetch(:direction, "DESC")
+    sort_direction_options = options_for_select(
       [
         ["Descending", "DESC"],
         ["Ascending", "ASC"]
       ],
       current_sort_direction_options,
     )
-  end
 
-  # DUP: laksjdlk2
-  def current_sort_direction_options
-    params.fetch(:sort, {}).fetch(:direction, "DESC")
+    form.select(
+      :direction,
+      sort_direction_options,
+      {},
+      { class: "select", onchange: "this.form.submit()" },
+    )
   end
 end
