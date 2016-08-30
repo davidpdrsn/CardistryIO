d888fa637695d1e2e9d1255b6236b76b7abfede5
bendobos <bendobos1@gmail.com>
2016-08-30 12:15:34 +0200

    Add actual data to widget

--- app/assets/javascripts/ajax_rating.js
+++ app/assets/javascripts/ajax_rating.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     })
     .success(function() {
       $(that).addClass("success");
-      $(that).text("Vote counted");
+      $(that).text("Rating counted");
 
       setTimeout(function() {
         $(that).text(buttonText);
