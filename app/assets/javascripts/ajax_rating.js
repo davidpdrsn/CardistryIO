e3ae41ab71a8eabfe5e875035da9fa3029f3aa91
bendobos <bendobos1@gmail.com>
2016-08-30 11:33:09 +0200

    Submit ratings via ajax

---
+++ app/assets/javascripts/ajax_rating.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(".rate-button").click(function() {
+    var valuesToSubmit = $(".rating-widget-form").serialize();
+    var that = $(this);
+    var buttonText = $(this).text();
+
+    $.ajax({
+      type: "POST",
+      url: $(".rating-widget-form").attr("action"),
+      data: valuesToSubmit
+    })
+    .success(function() {
+      $(that).addClass("success");
+      $(that).text("Vote counted");
+
+      setTimeout(function() {
+        $(that).text(buttonText);
+        $(that).removeClass("success");
+        $(that).addClass("active");
+      }, 2500);
+    });
+
+    return false;
+  })
+});
