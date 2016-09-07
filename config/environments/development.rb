91138b11ea10b3193571cdd80d01f3a61f3d5846
David Pedersen <david.pdrsn@gmail.com>
2016-09-07 07:53:46 +0200

    Make it easy to toggle caching on and off in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,12 @@
 Rails.application.configure do
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+  # config.action_controller.perform_caching = true
+  # config.cache_store = :redis_store
   config.cache_classes = false
+
   config.eager_load = false
   config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
   config.active_support.deprecation = :log
@@ -12,5 +16,4 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
   config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: "localhost:3000" }
-  config.cache_store = :redis_store
 end
