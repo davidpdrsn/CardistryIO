ba5a57525cd8e8ec9491ab45e38d2b683cbf4c9c
David Pedersen <david.pdrsn@gmail.com>
2016-09-07 09:55:20 +0200

    Disable base64 encoding thumbnails
    Wasn't faster... :(

--- app/jobs/load_video_thumbnail_job.rb
+++ app/jobs/load_video_thumbnail_job.rb
@@ -1,27 +1,7 @@
 class LoadVideoThumbnailJob < ApplicationJob
   def perform(video)
-    url = fetch_thumbnail_url(video: video)
-    video.update!(thumbnail_url: url)
-    unless Rails.env.test?
-      cache_image(video: video, url: url)
-    end
-  rescue JSON::ParserError
-  end
-
-  private
-
-  def fetch_thumbnail_url(video:)
     embeddable_video = EmbeddableVideo.new(video)
-    embeddable_video.fetch_thumbnail_url
-  end
-
-  def cache_image(video:, url:)
-    bytes = open_url(url)
-    Rails.cache.write(["thumbnail.base64", video], Base64.encode64(bytes))
-  end
-
-  def open_url(url)
-    uri = URI.parse(url)
-    Net::HTTP.get_response(uri).body
+    video.update!(thumbnail_url: embeddable_video.fetch_thumbnail_url)
+  rescue JSON::ParserError
   end
 end
