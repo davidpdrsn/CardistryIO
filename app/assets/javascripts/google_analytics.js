5120252c925c29a75a2b4ff43e3e79378f04b16d
David Pedersen <david.pdrsn@gmail.com>
2016-09-09 16:10:11 +0200

    Fix issue with google analytics in development

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -1,4 +1,4 @@
-if (USE_GOOGLE_ANALYTICS != null && USE_GOOGLE_ANALYTICS) {
+if (typeof(USE_GOOGLE_ANALYTICS) != "undefined" && USE_GOOGLE_ANALYTICS) {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
