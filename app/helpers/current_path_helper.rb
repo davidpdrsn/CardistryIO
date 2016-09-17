e41915493cf2c0f80c130f47d0e32e49b7c7a65a
David Pedersen <david.pdrsn@gmail.com>
2016-09-17 19:34:18 +0200

    Fix bug in `#current_path_class`
    /users/foo would match /users/bar, which is obviously wrong. The was
    just comparing the controller and actions

--- app/helpers/current_path_helper.rb
+++ app/helpers/current_path_helper.rb
@@ -1,7 +1,6 @@
 module CurrentPathHelper
   def current_path_class(path)
-    current_route = Rails.application.routes.recognize_path(path)
-    if should_be_highlighted?(path, current_route)
+    if should_be_highlighted?(path)
       "active-nav-item"
     end
   end
@@ -15,12 +14,7 @@ module CurrentPathHelper
 
   private
 
-  def should_be_highlighted?(path, current_route)
-    current_page?(path) || controller_action_matches?(current_route)
-  end
-
-  def controller_action_matches?(current_route)
-    params[:controller] == current_route[:controller] &&
-      params[:action] == current_route[:action]
+  def should_be_highlighted?(path)
+    request.env["PATH_INFO"] == path
   end
 end
